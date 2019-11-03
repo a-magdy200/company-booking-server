0c813d6688314773d9046ea79a3c226544fdb26a
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2019-11-03 19:55:34 +0200

    Configured server to accept cors

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const app = express();
 const port = 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 let accounts = [];
 let inspections = [];
